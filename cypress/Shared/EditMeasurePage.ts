608cd3366b48572a8c727cf4684a4ef90d613005
praveena.vasireddy <praveena.vasireddy@semanticbits.com>
2021-12-10 15:29:37 -0500

    Test case for Edit Measure.

---
+++ cypress/Shared/EditMeasurePage.ts
@@ -0,0 +1,6 @@
+export class EditMeasurePage {
+
+    public static readonly editMeasurePen = 'path'
+    public static readonly editMeasureTextBox = '[data-testid="inline-edit-input"]'
+    public static readonly saveEditedMeasureName = '.fa-check-circle > path'
+}
