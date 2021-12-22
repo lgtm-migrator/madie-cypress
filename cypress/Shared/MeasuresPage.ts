d36056c02ae5c887bdc4f9d5d3294926a4a397e9
cvasile001 <chris.vasile@semanticbits.com>
2021-12-22 15:01:49 -0500

    various updates, service side for edit measure error handling tests

---
+++ cypress/Shared/MeasuresPage.ts
@@ -0,0 +1,14 @@
+import {LandingPage} from "./LandingPage";
+
+export class MeasuresPage {
+
+    //let measureid = GetMeasureID(measurename)
+
+    // public static string editButton(measureid): {
+    //
+    //
+    //     let element = '[data-testid=edit-measure-' + measureID + ']'
+    //
+    //     return element
+    // }
+}
