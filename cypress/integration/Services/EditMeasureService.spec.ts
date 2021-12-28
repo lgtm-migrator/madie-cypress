3b0f7c50ead236ac99eb6c4ba4c0439d26251118
praveena.vasireddy <praveena.vasireddy@semanticbits.com>
2021-12-28 12:05:38 -0500

    Updated edit measure name validations service test case.

--- cypress/integration/Services/EditMeasureService.spec.ts
+++ cypress/integration/Services/EditMeasureService.spec.ts
@@ -1,7 +1,9 @@
+let measureName = ''
+
 describe('Edit Measure', () => {
 
-    let measureName = 'APITestMeasure' + Date.now()
     it('PUT Success 200 created', () => {
+        measureName = 'APITestMeasure' + Date.now()
         cy.request({
             url: '/api/measure',
             method: 'PUT',
@@ -10,7 +12,79 @@ describe('Edit Measure', () => {
             expect(response.status).to.eql(200)
         })
     })
+
+    it.only('Measure Name empty', () => {
+        measureName = ''
+        cy.request({
+            failOnStatusCode: false,
+            url: '/api/measure',
+            method: 'PUT',
+            body: {"id": "61c37324c3ea4928b42712f7","measureName": measureName}
+        }).then((response) => {
+            expect(response.status).to.eql(400)
+            expect(response.body.measureName).to.eql('Measure Name is required')
+        })
+    })
+
+    it('Measure Name empty', () => {
+        measureName = ''
+        cy.request({
+            failOnStatusCode: false,
+            url: '/api/measure',
+            method: 'PUT',
+            body: {"id": "61c37324c3ea4928b42712f7","measureName": measureName}
+        }).then((response) => {
+            expect(response.status).to.eql(400)
+            expect(response.body.measureName).to.eql('Measure Name is Required')
+        })
+    })
+
+    it('Measure Name does not contain at least 1 letter', () => {
+        measureName = '12343456456'
+        cy.request({
+            failOnStatusCode: false,
+            url: '/api/measure',
+            method: 'PUT',
+            body: {"id": "61c37324c3ea4928b42712f7","measureName": measureName}
+        }).then((response) => {
+            expect(response.status).to.eql(400)
+            expect(response.body.measureName).to.eql('Measure Name is Required')
+        })
+    })
+
+    it('Measure Name contains underscore', () => {
+        measureName = 'APITestMeasure_' + Date.now()
+        cy.request({
+            failOnStatusCode: false,
+            url: '/api/measure',
+            method: 'PUT',
+            body: {"id": "61c37324c3ea4928b42712f7","measureName": measureName}
+        }).then((response) => {
+            expect(response.status).to.eql(400)
+            expect(response.body.measureName).to.eql('Measure Name can not contain underscores')
+        })
+    })
+
+    it('Measure Name over 500 characters', () => {
+        measureName = 'qwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwerty' +
+            'qwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwerty' +
+            'qwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwerty' +
+            'qwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwerty' +
+            'qwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwqwertyqwertyqwertyqwertyqwertyq'
+        cy.request({
+            failOnStatusCode: false,
+            url: '/api/measure',
+            method: 'PUT',
+            body: {"id": "61c37324c3ea4928b42712f7","measureName": measureName}
+        }).then((response) => {
+            expect(response.status).to.eql(400)
+            expect(response.body.measureName).to.eql('Measure Name contains at least one letter and can not be ' +
+                'more than 500 characters')
+        })
+    })
+
     it('Save CQL to Measure', () => {
+        measureName = 'APITestMeasure' + Date.now()
         cy.request({
             url: '/api/measure',
             method: 'PUT',
