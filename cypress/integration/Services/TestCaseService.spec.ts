34563442abbca5c56c8a512344c8557b81403787
praveena.vasireddy <praveena.vasireddy@semanticbits.com>
2022-01-26 13:35:45 -0500

    MAT-3742 Test Case Service test

--- cypress/integration/Services/TestCaseService.spec.ts
+++ cypress/integration/Services/TestCaseService.spec.ts
@@ -1,28 +1,84 @@
 export {}
 
-let testCase = {
-    'id': 'test',
-    'name': 'testName',
-    'description': 'testDescription',
-    'createdBy': 'CV',
-    'lastModifiedBy': 'CV'
-}
+describe('Create Test Case', () => {
 
-describe('Test Case Service', () => {
+    before('Create Measure', () => {
 
-    it('Create Test Case, successful', () => {
-        cy.request({
-            url: '/api/test-case',
-            method: 'POST',
-            headers: {  'Content-Type': 'application/json'
+        cy.setAccessTokenCookie()
+
+        //Create New Measure
+        cy.getCookie('accessToken').then((accessToken) => {
+            cy.request({
+                url: '/api/measure',
+                headers: {
+                    authorization: 'Bearer ' + accessToken.value
+                },
+                method: 'POST',
+                body: {
+                    'measureName': 'TestMeasure' + Date.now(),
+                    'cqlLibraryName': 'TestCql' + Date.now(),
+                    'model': 'QI-Core',
+                    'measureScoring': 'Cohort'
+                }
+            }).then((response) => {
+                expect(response.status).to.eql(201)
+                expect(response.body.id).to.be.exist
+                cy.writeFile('cypress/downloads/measureId', response.body.id)
+            })
+        })
+    })
+
+    beforeEach('Set Access Token', () => {
+
+        cy.setAccessTokenCookie()
+
+    })
+
+    it('Add Test Case', () => {
+
+        //Add Test Case to the Measure
+        cy.getCookie('accessToken').then((accessToken) => {
+            cy.readFile('cypress/downloads/measureId').should('exist').then((id) => {
+                cy.request({
+                        url: '/api/measures/' + id + '/test-cases',
+                    headers: {
+                        authorization: 'Bearer ' + accessToken.value
                     },
-            body: testCase
-        }).then((response) => {
-            expect(response.status).to.eql(201)
-            expect(response.body.id).to.be.exist
+                    method: 'POST',
+                    body: {
+                        'name': "DENOMFail",
+                        'series': "WhenBP<120",
+                        'description': "DENOME pass Test HB <120"
+                    }
+                }).then((response) => {
+                    expect(response.status).to.eql(201)
+                    expect(response.body.id).to.be.exist
+                })
+            })
+        })
+    })
+
+    it('Get All Test Cases', () => {
+
+        cy.getCookie('accessToken').then((accessToken) => {
+            cy.readFile('cypress/downloads/measureId').should('exist').then((id) => {
+                cy.request({
+                    url: '/api/measures/' + id + '/test-cases',
+                    headers: {
+                        authorization: 'Bearer ' + accessToken.value
+                    },
+                    method: 'GET',
+
+                }).then((response) => {
+                    expect(response.status).to.eql(200)
+                    expect(response.body).to.be.exist
+                })
+            })
         })
     })
 })
+
+
 
 
 
