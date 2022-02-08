58d37e145878c082fc43ef7fd8cc646eaa5651ba
Brandon Welch <brandon.welch@semanticbits.com>
2022-02-08 15:19:24 -0600

    updated MeasuresPage file

--- cypress/Shared/MeasuresPage.ts
+++ cypress/Shared/MeasuresPage.ts
@@ -1,10 +1,7 @@
 
 
 export class MeasuresPage {
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
     public static clickEditforCreatedMeasure(): void {
         cy.readFile('cypress/downloads/measureId').should('exist').then((fileContents) => {
             cy.get('[data-testid=edit-measure-'+ fileContents +']').click()
