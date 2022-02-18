57838b80c6de7303057a64f93456ef5a9a7ecee3
Brandon Welch <brandon.welch@semanticbits.com>
2022-02-18 10:51:53 -0600

    updating files to test for CQL to ELM translation errors

--- cypress/Shared/MeasureGroupPage.ts
+++ cypress/Shared/MeasureGroupPage.ts
@@ -1,27 +1,15 @@
-import {MeasuresPage} from "../Shared/MeasuresPage"
-import {OktaLogin} from "../Shared/OktaLogin"
-import {CreateMeasurePage} from "../Shared/CreateMeasurePage"
-import {TestCasesPage} from "../Shared/TestCasesPage"
 import {EditMeasurePage} from "../Shared/EditMeasurePage"
+
 export class MeasureGroupPage {
-
-
-    //Navigate button to get to Measure Group page
-    public static readonly measureGroupTab = '[data-testid=measure-groups-tab]'
-
-    //Scoring drop-down box
-    public static readonly measureScoringDBox = '[data-testid=select-measure-scoring-groups]'
-
 
     //Measure Details
     public static readonly measureScoringUnit = "Ratio"
     public static readonly initialPopulation1 = ""
 
     public static clickMeasureGroupTab(): void {
-        
-            cy.get(this.measureGroupTab)
-            .invoke('removeAttr', 'target')
-            .click()
-
+        let measureGroupTabValue = EditMeasurePage.measureGroupsTab
+        cy.get(measureGroupTabValue)
+        .invoke('removeAttr', 'target')
+        .click()
     }
 }
