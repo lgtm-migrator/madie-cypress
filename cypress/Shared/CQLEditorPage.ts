b5b1e0f2af471bfb050c4f79c7cb7288a65c5a1d
Brandon Welch <brandon.welch@semanticbits.com>
2022-02-18 10:51:53 -0600

    updating files to test for CQL to ELM translation errors

--- cypress/Shared/CQLEditorPage.ts
+++ cypress/Shared/CQLEditorPage.ts
@@ -1,4 +1,20 @@
 export class CQLEditorPage {
+
+    //Navigate button to get to Measure Group page
+    public static readonly cqlEditorTab = '[data-testid=cql-editor-tab]'
+
+    //cql editor box on page
     public static readonly cqlEditorTextBox = '.ace_content'
+
+    //save button on page
     public static readonly cqlEditorSaveButton = '[data-testid="save-cql-btn"]'
+
+    public static clickMeasureGroupTab(): void {
+        
+        cy.get(this.cqlEditorTab)
+        .invoke('removeAttr', 'target')
+        .click()
+
+    }
+
 }
