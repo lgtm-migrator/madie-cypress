c2139a4558f5b82098f13a4b02c82721e44b8483
Brandon Welch <brandon.welch@semanticbits.com>
2022-03-08 12:14:01 -0600

    updated header file to no longer have unnecessary comments

--- cypress/Shared/Header.ts
+++ cypress/Shared/Header.ts
@@ -3,8 +3,6 @@ export class Header {
     public static readonly signOutButton = '.sc-pVTFL'
     //Main MADiE page
     public static readonly mainMadiePageButton = '.sc-dkPtRN.evWbfP > a > img'
-    //#main > nav > div > div.sc-dkPtRN.evWbfP > a > img
-    //<img src="https://dev-madie.hcqis.org/madie-layout/d5d2a62/440c877d753a4666ccb5.svg" alt="MADiE Logo">
     public static readonly newMeasureButton = '[data-testid=create-new-measure-button]'
 }
 
