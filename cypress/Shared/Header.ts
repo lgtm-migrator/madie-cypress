2ca1924eeb25a9a7bed76c0bf46f9d9e880528b0
praveena.vasireddy <praveena.vasireddy@semanticbits.com>
2022-03-08 16:27:19 -0500

    Updated CQL Library tab with id

--- cypress/Shared/Header.ts
+++ cypress/Shared/Header.ts
@@ -4,5 +4,6 @@ export class Header {
     //Main MADiE page
     public static readonly mainMadiePageButton = '.sc-dkPtRN.evWbfP > a > img'
     public static readonly newMeasureButton = '[data-testid=create-new-measure-button]'
+    public static readonly cqlLibraryTab = '#cql-library-main-nav-bar-tab'
 }
 
