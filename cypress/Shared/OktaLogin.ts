458654f20dc92be80d13faa384e1611295d8164a
cvasile001 <chris.vasile@semanticbits.com>
2022-03-14 16:19:57 -0400

    Updates for some timing issues, creating and editing test cases

--- cypress/Shared/OktaLogin.ts
+++ cypress/Shared/OktaLogin.ts
@@ -1,5 +1,6 @@
 import {Header} from "./Header"
 import {Environment} from "./Environment"
+import {MeasuresPage} from "./MeasuresPage";
 
 //MADiE OKTA Login Class
 export class OktaLogin {
@@ -11,15 +12,13 @@ export class OktaLogin {
 
     public static Login(): void {
 
-        //Navigate to Login page and Login with passed harp user and password
-        cy.intercept('POST', 'api/v1/authn').as('login')
-
         cy.visit('/', { onBeforeLoad: (win) => { win.sessionStorage.clear() } })
         cy.get(this.usernameInput, { timeout: 600000 }).should('be.visible')
         cy.get(this.usernameInput).type(Environment.credentials().harpUser)
         cy.get(this.passwordInput).type(Environment.credentials().password)
         cy.get(this.signInButton).click()
-        cy.wait('@login').its('response.statusCode').should('eq', 200)
+
+        cy.get(MeasuresPage.measureList).should('be.visible')
 
         cy.log('Login Successful')
 
@@ -27,11 +26,11 @@ export class OktaLogin {
 
     public static Logout(): void {
 
-        cy.intercept('GET', '/env-config/oktaConfig.json').as('logout')
-
         cy.get(Header.signOutButton).should('be.visible')
         cy.get(Header.signOutButton).click({force:true})
-        cy.wait('@logout').its('response.statusCode').should('eq', 200)
+        cy.location().should((loc) => {
+            expect(loc.pathname.toString()).to.contain('/login')
+        })
 
         cy.log('Logout Successful')
     }
