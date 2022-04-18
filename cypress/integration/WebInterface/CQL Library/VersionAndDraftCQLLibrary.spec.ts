a4ee8c1be7bd18c6ca7a813dae7302eaf8ab03eb
Brandon Welch <brandon.welch@semanticbits.com>
2022-04-18 14:02:18 -0500

    updated variable that holds pop-up messages related to version and drafts to be more generic and created test for creating multiple drafts

--- cypress/integration/WebInterface/CQL Library/VersionAndDraftCQLLibrary.spec.ts
+++ cypress/integration/WebInterface/CQL Library/VersionAndDraftCQLLibrary.spec.ts
@@ -39,7 +39,7 @@ describe('Add Version and Draft to CQL Library', () => {
 
         cy.get(CQLLibraryPage.versionLibraryRadioButton).eq(0).click()
         cy.get(CQLLibraryPage.createVersionContinueButton).click()
-        cy.get(CQLLibraryPage.successfulVersionMsg).should('contain.text', 'New version of CQL Library is Successfully created')
+        cy.get(CQLLibraryPage.VersionDraftMsgs).should('contain.text', 'New version of CQL Library is Successfully created')
         CQLLibraryPage.validateVersionNumber(CqlLibraryOne, versionNumber)
         cy.log('Version Created Successfully')
 
@@ -50,7 +50,7 @@ describe('Add Version and Draft to CQL Library', () => {
         CQLLibraryPage.clickDraftforCreatedLibrary()
         cy.get(CQLLibraryPage.updateDraftedLibraryTextBox).clear().type(updatedCqlLibraryName)
         cy.get(CQLLibraryPage.createDraftContinueBtn).click()
-        cy.get(CQLLibraryPage.successfulVersionMsg).should('contain.text', 'New Draft of CQL Library is Successfully created')
+        cy.get(CQLLibraryPage.VersionDraftMsgs).should('contain.text', 'New Draft of CQL Library is Successfully created')
         cy.get(CQLLibraryPage.cqlLibraryVersionList).should('contain', 'Draft 1.0.000')
         cy.log('Draft Created Successfully')
     })
@@ -65,6 +65,38 @@ describe('Add Version and Draft to CQL Library', () => {
        CQLLibraryPage.clickVersionforCreatedLibrary(true)
        cy.get(CQLLibraryPage.versionLibraryRadioButton).eq(0).click()
        cy.get(CQLLibraryPage.createVersionContinueButton).click()
-       cy.get(CQLLibraryPage.successfulVersionMsg).should('contain.text', 'User is unauthorized to create a version')
+       cy.get(CQLLibraryPage.VersionDraftMsgs).should('contain.text', 'User is unauthorized to create a version')
+   })
+
+   it.only('User cannot create a draft of a draft that already exists, while the version is still open', () => {
+
+        let versionNumber = '1.0.000'
+
+        CQLLibraryPage.clickVersionforCreatedLibrary()
+
+        cy.get(CQLLibraryPage.versionLibraryRadioButton).eq(0).click()
+        cy.get(CQLLibraryPage.createVersionContinueButton).click()
+        cy.get(CQLLibraryPage.VersionDraftMsgs).should('contain.text', 'New version of CQL Library is Successfully created')
+        CQLLibraryPage.validateVersionNumber(CqlLibraryOne, versionNumber)
+        cy.log('Version Created Successfully')
+
+        CQLLibraryPage.clickDraftforCreatedLibrary()
+        cy.get(CQLLibraryPage.updateDraftedLibraryTextBox).clear().type(updatedCqlLibraryName)
+        cy.get(CQLLibraryPage.createDraftContinueBtn).click()
+        cy.get(CQLLibraryPage.VersionDraftMsgs).should('contain.text', 'New Draft of CQL Library is Successfully created')
+        cy.get(CQLLibraryPage.cqlLibraryVersionList).should('contain', 'Draft 1.0.000')
+        cy.log('Draft Created Successfully')
+
+        CQLLibraryPage.clickDraftforCreatedLibrary()
+        cy.get(CQLLibraryPage.updateDraftedLibraryTextBox).clear().type(updatedCqlLibraryName+'1')
+        cy.get(CQLLibraryPage.createDraftContinueBtn).click()
+        cy.get(CQLLibraryPage.VersionDraftMsgs).should('contain.text', 'Cannot draft resource CQL Library. A draft already exists for the CQL Library Group.')
+
+
+
+   })
+
+   it('Draft of a draft can be created, after a version has been finalized', () =>{
+
    })
 })
