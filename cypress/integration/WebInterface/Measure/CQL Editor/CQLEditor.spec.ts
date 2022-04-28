c18da9fa7d03604070918ccc666790cca64e69eb
praveenavas <82400011+praveenavas@users.noreply.github.com>
2022-04-28 14:41:41 -0400

    minor updates (#231)

--- cypress/integration/WebInterface/Measure/CQL Editor/CQLEditor.spec.ts
+++ cypress/integration/WebInterface/Measure/CQL Editor/CQLEditor.spec.ts
@@ -77,7 +77,7 @@ describe('CQL Editor', () => {
 
     })
 
-    it.only('Verify errors appear on CQL Editor page and in the CQL Editor object, on save and on tab / page load, when included library is not found', () => {
+    it('Verify errors appear on CQL Editor page and in the CQL Editor object, on save and on tab / page load, when included library is not found', () => {
 
         //Click on Edit Measure
         MeasuresPage.clickEditforCreatedMeasure()
