9c5208b613d31b34f002607a744c1e30111f7656
praveena.vasireddy <praveena.vasireddy@semanticbits.com>
2022-05-06 09:54:19 -0400

    MAT-4173 Measure Highlighting and few more updates

---
+++ cypress/integration/WebInterface/Test Cases/MeasureHighlighting.spec.ts
@@ -0,0 +1,148 @@
+import {TestCaseJson} from "../../../Shared/TestCaseJson"
+import {CreateMeasurePage} from "../../../Shared/CreateMeasurePage"
+import {OktaLogin} from "../../../Shared/OktaLogin"
+import {MeasureGroupPage} from "../../../Shared/MeasureGroupPage"
+import {EditMeasurePage} from "../../../Shared/EditMeasurePage"
+import {TestCasesPage} from "../../../Shared/TestCasesPage"
+import {Utilities} from "../../../Shared/Utilities"
+import {MeasuresPage} from "../../../Shared/MeasuresPage"
+
+let measureName = 'TestMeasure' + Date.now()
+let CqlLibraryName = 'TestLibrary' + Date.now()
+let measureScoringArray = ['Ratio', 'Cohort', 'Continuous Variable', 'Proportion']
+let testCaseTitle = 'test case title'
+let testCaseDescription = 'DENOMFail' + Date.now()
+let testCaseSeries = 'SBTestSeries'
+let testCaseJson = TestCaseJson.TestCaseJson_Valid
+
+describe('Measure Highlighting', () => {
+
+
+    beforeEach('Create Measure and Login', () => {
+
+        let randValue = (Math.floor((Math.random() * 1000) + 1))
+        let newMeasureName = measureName + randValue
+        let newCqlLibraryName = CqlLibraryName + randValue
+
+        //Create New Measure
+        CreateMeasurePage.CreateQICoreMeasureAPI(newMeasureName, newCqlLibraryName, measureScoringArray[0])
+
+        OktaLogin.Login()
+
+    })
+    afterEach('Logout', () => {
+
+        OktaLogin.Logout()
+
+    })
+
+    afterEach('Clean up', () => {
+
+        let randValue = (Math.floor((Math.random() * 1000) + 1))
+        let newCqlLibraryName = CqlLibraryName + randValue
+
+        Utilities.deleteMeasure(measureName, newCqlLibraryName, measureScoringArray[0])
+
+    })
+
+    it('Execute single Test Case and verify Measure highlighting', () => {
+
+        //Add Measure Group
+        MeasureGroupPage.createMeasureGroupforRatioMeasure()
+
+        //Navigate to Test Cases page and add Test Case details
+        cy.get(EditMeasurePage.testCasesTab).click()
+        cy.get(TestCasesPage.newTestCaseButton).should('be.visible')
+        cy.get(TestCasesPage.newTestCaseButton).should('be.enabled')
+        cy.get(TestCasesPage.newTestCaseButton).click()
+
+        cy.get(TestCasesPage.testCasePopulationList).should('be.visible')
+
+        cy.wait(2000)
+
+        cy.get(TestCasesPage.testCaseTitle).should('be.visible')
+        cy.get(TestCasesPage.testCaseTitle).should('be.enabled')
+        cy.get(TestCasesPage.testCaseTitle).type(testCaseTitle, { force: true })
+        cy.get(TestCasesPage.testCaseDescriptionTextBox).type(testCaseDescription)
+        cy.get(TestCasesPage.testCaseSeriesTextBox).type(testCaseSeries)
+        cy.get(TestCasesPage.existingTestCaseSeriesDropdown).click()
+
+        //Add json to the test case
+        cy.get(TestCasesPage.aceEditor).type(testCaseJson)
+
+        TestCasesPage.clickCreateTestCaseButton()
+
+        TestCasesPage.clickEditforCreatedTestCase()
+
+        cy.get(TestCasesPage.runTestButton).click()
+
+        cy.get(TestCasesPage.testCalculationResults).should('contain', 'Population Group: population-group-1')
+
+        cy.get(TestCasesPage.testCalculationResultsLineTwo).should('contain.text','define "ipp":\n' +
+            '  exists ["Encounter"] E where E.period.start during "Measurement Period"')
+    })
+
+    it('Verify error message when the Test Case Json is empty', () => {
+
+        //Add Measure Group
+        MeasureGroupPage.createMeasureGroupforRatioMeasure()
+
+        //Navigate to Test Cases page and add Test Case details
+        cy.get(EditMeasurePage.testCasesTab).click()
+        cy.get(TestCasesPage.newTestCaseButton).should('be.visible')
+        cy.get(TestCasesPage.newTestCaseButton).should('be.enabled')
+        cy.get(TestCasesPage.newTestCaseButton).click()
+
+        cy.get(TestCasesPage.testCasePopulationList).should('be.visible')
+
+        cy.wait(2000)
+
+        cy.get(TestCasesPage.testCaseTitle).should('be.visible')
+        cy.get(TestCasesPage.testCaseTitle).should('be.enabled')
+        cy.get(TestCasesPage.testCaseTitle).type(testCaseTitle, { force: true })
+        cy.get(TestCasesPage.testCaseDescriptionTextBox).type(testCaseDescription)
+        cy.get(TestCasesPage.testCaseSeriesTextBox).type(testCaseSeries)
+        cy.get(TestCasesPage.existingTestCaseSeriesDropdown).click()
+
+        TestCasesPage.clickCreateTestCaseButton()
+
+        TestCasesPage.clickEditforCreatedTestCase()
+
+        cy.get(TestCasesPage.runTestButton).click()
+
+        cy.get(TestCasesPage.testCalculationError).should('contain.text', 'Cannot read properties of null (reading \'entry\')')
+    })
+
+    it('Verify error message when the Measure group is not added', () => {
+
+        //Click on Edit Measure
+        MeasuresPage.clickEditforCreatedMeasure()
+
+        //Navigate to Test Cases page and add Test Case details
+        cy.get(EditMeasurePage.testCasesTab).click()
+        cy.get(TestCasesPage.newTestCaseButton).should('be.visible')
+        cy.get(TestCasesPage.newTestCaseButton).should('be.enabled')
+        cy.get(TestCasesPage.newTestCaseButton).click()
+
+        cy.get(TestCasesPage.testCasePopulationList).should('be.visible')
+
+        cy.wait(2000)
+
+        cy.get(TestCasesPage.testCaseTitle).should('be.visible')
+        cy.get(TestCasesPage.testCaseTitle).should('be.enabled')
+        cy.get(TestCasesPage.testCaseTitle).type(testCaseTitle, { force: true })
+        cy.get(TestCasesPage.testCaseDescriptionTextBox).type(testCaseDescription)
+        cy.get(TestCasesPage.testCaseSeriesTextBox).type(testCaseSeries)
+        cy.get(TestCasesPage.existingTestCaseSeriesDropdown).click()
+
+        TestCasesPage.clickCreateTestCaseButton()
+
+        TestCasesPage.clickEditforCreatedTestCase()
+
+        cy.get(TestCasesPage.runTestButton).click()
+
+        cy.get(TestCasesPage.testCalculationError).should('contain.text', 'Cannot read properties of null (reading \'map\')')
+    })
+})
+
+
