84cc95d00879768dfa13605ea3d07bb83eda4a92
praveenavas <82400011+praveenavas@users.noreply.github.com>
2022-09-14 10:37:07 -0400

    MeasureListPagination.spec fix (#451)
    * MAT-4639
    
    * Fixed MeasureListPagination.spec
    
    * MeasureListPagination.spec fix

--- cypress/integration/WebInterface/Measure/CreateMeasure/MeasureListPagination.spec.ts
+++ cypress/integration/WebInterface/Measure/CreateMeasure/MeasureListPagination.spec.ts
@@ -7,6 +7,7 @@ let measureIds = []
 const now = require('dayjs')
 let mpStartDate = now().subtract('1', 'year').format('YYYY-MM-DD')
 let mpEndDate = now().format('YYYY-MM-DD')
+let versionIdPath = 'cypress/fixtures/versionId'
 
 describe('Measure List Pagination', () => {
 
@@ -22,28 +23,31 @@ describe('Measure List Pagination', () => {
             CqlLibraryName [i] = 'TestLibrary' + i + Date.now()
 
             cy.getCookie('accessToken').then((accessToken) => {
+                cy.readFile(versionIdPath).should('exist').then((vId) => {
 
-                cy.request({
-                    url: '/api/measure',
-                    headers: {
-                        authorization: 'Bearer ' + accessToken.value
-                    },
-                    method: 'POST',
-                    body: {
-                        'measureName': measureName[i],
-                        'cqlLibraryName': CqlLibraryName[i],
-                        'model': 'QI-Core v4.1.1',
-                        "ecqmTitle": "eCQMTitle",
-                        "measurementPeriodStart": mpStartDate,
-                        "measurementPeriodEnd": mpEndDate
-                    }
-                }).then((response) => {
-                    expect(response.status).to.eql(201)
-                    expect(response.body.id).to.be.exist
-                    measureIds [i] = response.body.id
-                    fileContents = fileContents + measureIds[i] + ','
-                    cy.writeFile('cypress/fixtures/measureId', fileContents)
+                    cy.request({
+                        url: '/api/measure',
+                        headers: {
+                            authorization: 'Bearer ' + accessToken.value
+                        },
+                        method: 'POST',
+                        body: {
+                            'measureName': measureName[i],
+                            'cqlLibraryName': CqlLibraryName[i],
+                            'model': 'QI-Core v4.1.1',
+                            "ecqmTitle": "eCQMTitle",
+                            "versionId": vId,
+                            "measurementPeriodStart": mpStartDate,
+                            "measurementPeriodEnd": mpEndDate
+                        }
+                    }).then((response) => {
+                        expect(response.status).to.eql(201)
+                        expect(response.body.id).to.be.exist
+                        measureIds [i] = response.body.id
+                        fileContents = fileContents + measureIds[i] + ','
+                        cy.writeFile('cypress/fixtures/measureId', fileContents)
 
+                    })
                 })
             })
         }
@@ -58,36 +62,43 @@ describe('Measure List Pagination', () => {
         let idsList = ''
 
         cy.readFile('cypress/fixtures/measureId').should('exist').then((fileContents) => {
+            cy.readFile(versionIdPath).should('exist').then((vId) => {
 
-            idsList = fileContents.split(',')
+                idsList = fileContents.split(',')
 
-            for(let j=0; j < idsList.length-1; j++) {
+                for (let j = 0; j < idsList.length - 1; j++) {
 
-                cy.getCookie('accessToken').then((accessToken) => {
+                    cy.getCookie('accessToken').then((accessToken) => {
 
-                    let id = idsList[j]
+                        let id = idsList[j]
 
-                    cy.log('ID is : ' + id)
+                        cy.log('ID is : ' + id)
 
-                    cy.request({
-                        url: '/api/measures/' + id,
-                        method: 'PUT',
-                        headers: {
-                            Authorization: 'Bearer ' + accessToken.value
-                        },
-                        body: {
-                            "id": id, "measureName": measureName[j], "cqlLibraryName": CqlLibraryName[j] + 1,
-                            "model": 'QI-Core v4.1.1', "ecqmTitle": "eCQMTitle", "active": false, "measurementPeriodStart": mpStartDate,
-                            "measurementPeriodEnd": mpEndDate
-                        }
+                        cy.request({
+                            url: '/api/measures/' + id,
+                            method: 'PUT',
+                            headers: {
+                                Authorization: 'Bearer ' + accessToken.value
+                            },
+                            body: {
+                                "id": id,
+                                "measureName": measureName[j],
+                                "cqlLibraryName": CqlLibraryName[j] + 1,
+                                "model": 'QI-Core v4.1.1',
+                                "versionId": vId,
+                                "ecqmTitle": "eCQMTitle",
+                                "active": false,
+                                "measurementPeriodStart": mpStartDate,
+                                "measurementPeriodEnd": mpEndDate
+                            }
 
-                    }).then((response) => {
-                        expect(response.status).to.eql(200)
-                        expect(response.body).to.eql("Measure updated successfully.")
+                        }).then((response) => {
+                            expect(response.status).to.eql(200)
+                            expect(response.body).to.eql("Measure updated successfully.")
+                        })
                     })
-                })
-            }
-
+                }
+            })
         })
 
         OktaLogin.Logout()
@@ -114,6 +125,5 @@ describe('Measure List Pagination', () => {
         cy.get(MeasuresPage.paginationLimitEquals25).click( {force:true} )
         //Verify pagination limit after change
         cy.get(MeasuresPage.paginationLimitSelect).should('contain', '25')
-
     })
 })
