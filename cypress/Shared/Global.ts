d5fa0c7f4dc570a7e3874e4cde4c9d87093d3189
cvasile001 <chris.vasile@semanticbits.com>
2022-10-31 11:02:56 -0400

    4650 and 4651

---
+++ cypress/Shared/Global.ts
@@ -0,0 +1,7 @@
+export class Global {
+
+    //dirty modal
+    public static readonly dirtCheckModal = '[class="MuiPaper-root MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation24 MuiDialog-paper MuiDialog-paperScrollPaper MuiDialog-paperWidthSm MuiDialog-paperFullWidth css-mbdu2s"]'
+    public static readonly keepWorkingCancel = '[data-testid="discard-dialog-cancel-button"]'
+    public static readonly discardChangesContinue = '[data-testid="discard-dialog-continue-button"]'
+}
