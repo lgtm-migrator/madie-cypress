97601d9935815f2b95f2c46aad8e1401f3ffeade
cvasile001 <chris.vasile@semanticbits.com>
2022-11-08 12:03:44 -0500

    Fix for MeasureObservations.spec.ts

--- cypress/integration/WebInterface/Measure/Measure Group/MeasureObservations.spec.ts
+++ cypress/integration/WebInterface/Measure/Measure Group/MeasureObservations.spec.ts
@@ -380,7 +380,7 @@ describe('Measure Observations and Stratification -- non-owner tests', () => {
         cy.get(MeasureGroupPage.cvMeasureObservation).click()
         cy.get(MeasureGroupPage.measureObservationSelect).should('exist')
         cy.get(MeasureGroupPage.measureObservationSelect).should('be.visible')
-        cy.get(MeasureGroupPage.measureObservationSelect).wait(1000).eq(0).wait(1000).click() //select ToCode
+        Utilities.dropdownSelect(MeasureGroupPage.measureObservationSelect, 'ToCode')
         cy.get(MeasureGroupPage.cvAggregateFunction).click()
         cy.get(MeasureGroupPage.aggregateFunctionCount).click()
 
